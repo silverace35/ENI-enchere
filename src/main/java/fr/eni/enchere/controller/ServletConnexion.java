d734a07103d0d782ec1eafedd117bb56fdd89bd1
Kevin Le Gall <legall.kevin2023@campus-eni.fr>
2023-05-09 15:43:06 +0200

    ajout connexion js p et servletconnexion

---
+++ src/main/java/fr/eni/enchere/controller/ServletConnexion.java
@@ -0,0 +1,62 @@
+package fr.eni.enchere.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class ServletConnexion
+ */
+@WebServlet("/ServletConnexion")
+public class ServletConnexion extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletConnexion() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd = request.getRequestDispatcher("/connexion.jsp");
+		rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			// identifiant peut soit etre le pseudo soit le mail
+			String idUser = request.getParameter("identifiant");
+	        String pwdUser = request.getParameter("motDePasse");
+	        System.out.println("id : " + idUser);
+	        System.out.println("mot de passe : " + pwdUser);
+	        //TODO : gerer les erreurs d'entrées utilisateur
+	        if (idUser != null && pwdUser != null) {
+	            //TODO utiliser la BLL pour vérifier la validité des identifiants
+	        	//SI VALID : httpSession.setAttribute("IdUser", idUser);
+	            // TODO : créer cookie pour se souvenir de moi
+	            RequestDispatcher rd = request.getRequestDispatcher("/profil.jsp");
+	    		rd.forward(request, response);
+	        } else {
+	        	throw new Exception("Identifiant ou mot de passe invalide");
+	        }
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		 
+	}
+
+}
---
+++ src/main/webapp/connexion.jsp
@@ -0,0 +1,33 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Se connecter | ENI-enchere</title>
+</head>
+<body>
+	<nav>	
+		<a href="index.jsp">ENI-Enchères</a>
+	</nav>
+	
+	<form action="<%=request.getContextPath()%>/ServletAjoutUtilisateur" method="POST">
+		<label for="identifiant">Identifiant</label>
+		<input id="identifiant" name="identifiant" type="text">
+		
+		<label for="motDePasse">Mot de passe</label>
+		<input id="motDePasse" name="motDePasse" type="password">
+		
+		<div class="conn-details">		
+			<button type="submit">Connexion</button>
+			<label for="souvenir">Se souvenir de moi</label>
+			<input id="souvenir" name="souvenir" type="checkbox">
+			<a href="#">Mot de passe oublié</a>
+			<!-- TODO : ajouter un message de mail envoyé lors du click du lien -->
+		</div>
+	</form>
+	
+	<a href="ServletInscription">Créer un compte</a>
+
+</body>
+</html>
