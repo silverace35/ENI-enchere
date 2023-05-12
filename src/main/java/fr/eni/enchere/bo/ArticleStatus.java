8fb0dc3231589d3216022ecf2802c6da902ba094
Zian Woreth <zworeth2023@131SE7-261MMS3>
2023-05-12 11:14:43 +0200

    Creation de la BO ArticleVendu et de l'enum pour ses status

---
+++ src/main/java/fr/eni/enchere/bo/ArticleStatus.java
@@ -0,0 +1,9 @@
+package fr.eni.enchere.bo;
+
+public enum ArticleStatus {
+	CR, //Created
+	EC, //En cours
+	ET, //Enchere terminer
+	RT, //Retrait effectuer
+	DS, //Cas ou le proprietaire de la vente est desactiver ?
+}
