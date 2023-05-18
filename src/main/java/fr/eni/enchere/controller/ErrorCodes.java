668e3ad94fa7f5baf83cc50aa90374f85d21ab30
Teck Tea <teckos@hotmail.fr>
2023-05-18 13:15:58 +0200

    Fix pattern plus permissifs

--- src/main/java/fr/eni/enchere/controller/ErrorCodes.java
+++ src/main/java/fr/eni/enchere/controller/ErrorCodes.java
@@ -2,14 +2,14 @@ package fr.eni.enchere.controller;
 
 public enum ErrorCodes {
 	IDORPASSWORD("Identifiant ou mot de passe incorrect"),
-	PSEUDO("Caractères alphanumériques requis.", "^[\\w-]{2,30}$"),
-	NOM("Caractères alphanumériques requis.", "^[\\p{L}- \\']{2,30}$"),
-	PRENOM("Caractères alphanumériques requis.", "^[a-z A-Z\\'\\-]{2,30}$"),
+	PSEUDO("Caractères alphanumériques requis.", "^.{2,30}$"),
+	NOM("Caractères alphanumériques requis.", "^.{2,30}$"),
+	PRENOM("Caractères alphanumériques requis.", "^.{2,30}$"),
 	EMAIL("Type de format possible xxxx-xxxx@xx-xx.xxx.", "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$"),
 	TEL("+33123123112 ou 0110203040.", "(^\\+{1}+[3]{2}+[0-9]{9}$)|(^0{1}+[0-9]{9}$)"),
-	RUE("Caractères alphanumériques requis.", "^(?=.*[\\p{L}- 0-9.]).{2,30}$"),
+	RUE("Caractères alphanumériques requis.", "^.{2,30}$"),
 	CODEPOSTAL("Code postal incorrect.", "^[0-9]{5}$"),
-	VILLE("Caractères alphanumériques requis.", "^[\\p{L}\\'\\-]{2,50}$"),
+	VILLE("Caractères alphanumériques requis.", "^.{2,50}$"),
 	PWDUSER("8 caractères min. (au moins 1 majuscule, 1 chiffre, 1 caractère spécial)", "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&+=*])(?=\\S+$).{8,30}$"),
 	CONFPWDUSER("8 caractères min. (au moins 1 majuscule, 1 chiffre, 1 caractère spécial)", "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&+=*])(?=\\S+$).{8,30}$"),
 	PASSWORDMISSMATCH("Les mots de passe ne correspondent pas."),
@@ -17,7 +17,7 @@ public enum ErrorCodes {
 	PSEUDO_ALREADY_EXIST("Le pseudo existe déjà."),
 	EMAIL_ALREADY_EXIST("l'email existe déjà."),
 	SQL_ERROR("Connexion à la base impossible."),
-	DESCRIPTION("Caractères non autorisés.", "^(?=.*[\\p{L}- 0-9.:!%]).{2,300}$"),
+	DESCRIPTION("Caractères non autorisés.", "^.{2,300}$"),
 	PRIX_INITIAL("Saisie invalide."),
 	DATES_IMP("La date de fin doit être postérieure à celle de début."),
 	DATETIMEFORMAT("Format de date invalide aaaa-mm-jj hh:mm","")
